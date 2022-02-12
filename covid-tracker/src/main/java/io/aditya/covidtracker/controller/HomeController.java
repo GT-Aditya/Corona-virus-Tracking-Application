606866256554e241790b78e781c269c59f0f2b3f
Aditya <98248389+GT-Aditya@users.noreply.github.com>
2022-02-13 00:02:03 +0530

    Update HomeController.java

--- covid-tracker/src/main/java/io/aditya/covidtracker/controller/HomeController.java
+++ covid-tracker/src/main/java/io/aditya/covidtracker/controller/HomeController.java
@@ -27,7 +27,7 @@ public class HomeController {
         model.addAttribute("totalCases", totalCases);
         model.addAttribute("totalNewCases", totalNewCases);
 
-        return "home";
+        return "index";
     }
     
 }
